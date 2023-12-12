036dcff58cec67417337254f3b828a8c77399b17
oumar <alphaoumareclipse@gmail.com>
2023-12-12 23:01:52 +0100

    Database seeder for bootcamps

---
+++ seeder.js
@@ -0,0 +1,34 @@
+
+const connectDB = require('./config/database')
+const Bootcamp = require('./models/BootcampModel')
+const fs = require('fs')
+require('colors')
+const bootcamps = JSON.parse(fs.readFileSync(`./_data/bootcamps.json`, 'utf-8'))
+connectDB()
+
+const importData = async () => {
+    try {
+        await Bootcamp.create(bootcamps)
+        console.log(`Data imported successfully`.cyan.bold)
+        process.exit()
+    } catch (err) {
+        console.error(`${err}`.red.bold)
+    }
+}
+
+const deleteData = async () => {
+    try {
+        await Bootcamp.deleteMany()
+        console.log(`Data deleted successfully`.cyan.bold)
+        process.exit()
+    } catch (err) {
+        console.error(`${err}`.red.bold)
+    }
+}
+
+if(process.argv[2] == '-i'){
+    importData()
+} else if(process.argv[2] == '-d'){
+    deleteData()
+}
+
